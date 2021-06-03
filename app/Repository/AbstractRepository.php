64a496795d20702c14a3a46dc91dd16884b23709
JucieLima <jucielima@gmail.com>
2021-06-03 06:53:39 -0300

    Adicionando AbstractRepository

---
+++ app/Repository/AbstractRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+abstract class AbstractRepository
+{
+    /**
+     * @var Model
+     */
+    private $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function selectFilter($filters)
+    {
+        $this->model = $this->model->selectRaw($filters);
+    }
+
+    public function selectConditions($conditions){
+        $expressions = explode(';', $conditions);
+
+        foreach ($expressions as $expression){
+            $filter = explode(':', $expression);
+            $this->model = $this->model->where($filter[0], $filter[1], $filter[2]);
+        }
+    }
+
+    public function getResult(){
+        return $this->model;
+    }
+}
--- app/Repository/ProductRepository.php
+++ app/Repository/ProductRepository.php
@@ -4,36 +4,7 @@
 namespace App\Repository;
 
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
-
-class ProductRepository
+class ProductRepository extends AbstractRepository
 {
-    /**
-     * @var Model
-     */
-    private $model;
 
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
-    }
-
-    public function selectFilter($filters)
-    {
-        $this->model = $this->model->selectRaw($filters);
-    }
-
-    public function selectConditions($conditions){
-        $expressions = explode(';', $conditions);
-
-        foreach ($expressions as $expression){
-            $filter = explode(':', $expression);
-            $this->model = $this->model->where($filter[0], $filter[1], $filter[2]);
-        }
-    }
-
-    public function getResult(){
-        return $this->model;
-    }
 }
